d2ef0fe9ef47c4bb89599c5788dfe9dc4b2e067e
Acerizm <34855234+Acerizm@users.noreply.github.com>
2018-11-28 12:04:28 +0800

    Player Class

---
+++ EnginePart1/player.h
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef _PLAYER_H             // prevent multiple definitions if this 
+#define _PLAYER_H             // ..file is included in more than one place
+#define WIN32_LEAN_AND_MEAN
+
+#include "game.h"
+#include "textureManager.h"
+#include "image.h"
+#include "lastManStanding.h"
+#include <vector>
+using namespace std;
+
+/*
+
+*/
+class Player : public LastManStanding 
+{
+	
+
+}
+
+
+
+#endif
